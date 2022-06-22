ae35491692e4c8e388f22c533ecf5004e71848d3
TheoPierne <theo.pierne2@gmail.com>
2022-06-23 01:46:24 +0200

    chore: modify build script and add npm run command for it

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const child_process = require('node:child_process');
-const path = require('node:path');
+const { join } = require('node:path');
 
 const paths = [
 'C:/Users/theop/Desktop/spot-sdk/spot-sdk-py/spot-sdk-3.1.2/protos',
@@ -16,14 +16,20 @@ const paths = [
 
 const output = './';
 
-// eslint-disable-next-line
-const command = `grpc_tools_node_protoc --js_out=import_style=commonjs,binary:${output} --grpc_out=grpc_js:${output} --proto_path=${paths.map(e => path.join(e)).join(' ')}`;
+const options = [
+`--js_out=import_style=commonjs,binary:${output}`,
+`--grpc_out=grpc_js:${output}`,
+`--proto_path=${paths.map(e => join(e)).join(' ')}`,
+];
+
+const command = `grpc_tools_node_protoc ${options.join(' ')}`;
 
 child_process.exec(command, (err, stdout, stderr) => {
 	if (err) {
 		console.error(err);
 		return;
 	}
-	console.log(stdout);
+	if (stderr) console.log(stderr);
+	console.log('BUILD COMPLETE!');
 });
 
