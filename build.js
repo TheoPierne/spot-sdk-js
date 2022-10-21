accffbe974362404df38cd9c6c1b167cff255b56
TheoPierne <theo.pierne2@gmail.com>
2022-10-21 08:24:33 +0200

    Modify build script

--- build.js
+++ build.js
@@ -1,35 +1,37 @@
 'use strict';
 
 const child_process = require('node:child_process');
-const { join } = require('node:path');
+const { join, resolve } = require('node:path');
+
+const version = '3.2.0';
 
 const paths = [
-'C:/Users/theop/Desktop/spot-sdk/spot-sdk-py/spot-sdk-3.1.2/protos',
-'C:/Users/theop/Desktop/spot-sdk/spot-sdk-py/spot-sdk-3.1.2/protos/bosdyn/api/*.proto',
-'C:/Users/theop/Desktop/spot-sdk/spot-sdk-py/spot-sdk-3.1.2/protos/bosdyn/api/spot_cam/*.proto',
-'C:/Users/theop/Desktop/spot-sdk/spot-sdk-py/spot-sdk-3.1.2/protos/bosdyn/api/spot/*.proto',
-'C:/Users/theop/Desktop/spot-sdk/spot-sdk-py/spot-sdk-3.1.2/protos/bosdyn/api/mission/*.proto',
-'C:/Users/theop/Desktop/spot-sdk/spot-sdk-py/spot-sdk-3.1.2/protos/bosdyn/api/graph_nav/*.proto',
-'C:/Users/theop/Desktop/spot-sdk/spot-sdk-py/spot-sdk-3.1.2/protos/bosdyn/api/docking/*.proto',
-'C:/Users/theop/Desktop/spot-sdk/spot-sdk-py/spot-sdk-3.1.2/protos/bosdyn/api/auto_return/*.proto',
+  `../spot-sdk-py/spot-sdk-${version}/protos`,
+  `../spot-sdk-py/spot-sdk-${version}/protos/bosdyn/api/*.proto`,
+  `../spot-sdk-py/spot-sdk-${version}/protos/bosdyn/api/autowalk/*.proto`,
+  `../spot-sdk-py/spot-sdk-${version}/protos/bosdyn/api/spot_cam/*.proto`,
+  `../spot-sdk-py/spot-sdk-${version}/protos/bosdyn/api/spot/*.proto`,
+  `../spot-sdk-py/spot-sdk-${version}/protos/bosdyn/api/mission/*.proto`,
+  `../spot-sdk-py/spot-sdk-${version}/protos/bosdyn/api/graph_nav/*.proto`,
+  `../spot-sdk-py/spot-sdk-${version}/protos/bosdyn/api/docking/*.proto`,
+  `../spot-sdk-py/spot-sdk-${version}/protos/bosdyn/api/auto_return/*.proto`,
 ];
 
 const output = './';
 
 const options = [
-`--js_out=import_style=commonjs,binary:${output}`,
-`--grpc_out=grpc_js:${output}`,
-`--proto_path=${paths.map(e => join(e)).join(' ')}`,
+  `--js_out=import_style=commonjs,binary:${output}`,
+  `--grpc_out=grpc_js:${output}`,
+  `--proto_path=${paths.map(e => resolve(join(e))).join(' ')}`,
 ];
 
 const command = `grpc_tools_node_protoc ${options.join(' ')}`;
 
 child_process.exec(command, (err, stdout, stderr) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	if (stderr) console.log(stderr);
-	console.log('BUILD COMPLETE!');
+  if (err) {
+    console.error(err);
+    return;
+  }
+  if (stderr) console.log(stderr);
+  console.log('BUILD COMPLETE!');
 });
-
